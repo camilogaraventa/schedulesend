af5d2a78c49da2e39c27075b92beb283f45610b0
cgaraventa <cgaraventa@szicom.com.ar>
2021-01-07 10:37:45 -0300

    Initial commit

---
+++ ScheduleSend/Program.cs
@@ -0,0 +1,51 @@
+﻿using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+
+namespace ScheduleSend
+{
+    public static class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var services = new ServiceCollection();
+
+            services.AddMassTransit(x =>
+            {
+                x.AddRabbitMqMessageScheduler();
+
+                x.UsingRabbitMq((context, cfg) =>
+                {
+                    cfg.Host("localhost", "test", h =>
+                    {
+                        h.Username("test");
+                        h.Password("test");
+                    });
+
+                    cfg.UseDelayedExchangeMessageScheduler();
+
+                    cfg.ConfigureEndpoints(context);
+                });
+            });
+
+            var provider = services.BuildServiceProvider();
+
+            var scheduler = provider.GetRequiredService<IMessageScheduler>();
+
+            await scheduler.ScheduleSend<Mensaje>(
+                new Uri("queue:Mensaje"),
+                DateTime.Now + TimeSpan.FromSeconds(30), new Mensaje
+                {
+                    Fecha = DateTime.Now,
+                    Id = Guid.NewGuid()
+                });
+        }
+    }
+
+    public class Mensaje
+    {
+        public DateTime Fecha { get; set; }
+        public Guid Id { get; set; }
+    }
+}
